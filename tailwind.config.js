424109db6f940c001755e03c91d98b295c9b802d
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-07-18 14:48:28 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        14: "3.5rem",
+        125: "125px",
+      },
+      colors: {
+        hoverspt: "#18D760",
+        "purple-main": "#2D46B9",
+        "green-main": "#1ED760",
+      },
+      backgroundImage: {
+        "spotify-theme": "url('../img/bursts.svg')",
+        "spotify-theme-mobile": "url('../img/bursts-mobile.svg')",
+      },
+      backgroundSize: {
+        "175%": "175%",
+        "195%": "195%",
+      },
+      backgroundPosition: {
+        banner: "46% 4%",
+        "banner-mobile": "top 25% center",
+      },
+      fontSize: {
+        "9xl": "9rem",
+      },
+    },
+  },
+  plugins: [],
+};
